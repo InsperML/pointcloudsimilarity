89a4f0701f0e7f25c7951f986ca5671562c336dd
Fabio Ayres <fabioja@insper.edu.br>
2026-06-05 17:46:03 +0000

    bugfix: test was not covering similarity = None

--- tests/test_cka.py
+++ tests/test_cka.py
@@ -1,22 +1,23 @@
+import numpy as np
+
 from pointcloudsimilarity import cka
 
-import numpy as np
 
 def test_cka_rbf_sigma_small():
     pc1 = np.random.randn(500, 30)
     pc2 = pc1 + 0.05 * np.random.randn(500, 30)
     sim = cka.CKASimilarity(kernel='rbf', sigma=1e-10)
     similarity = sim(pc1, pc2)
-    
-    
+
+    assert similarity is not None
     assert similarity > 0.8
-    
+
+
 def test_cka_rbf_sigma_large():
     pc1 = np.random.randn(5, 3)
     pc2 = pc1 + 0.05 * np.random.randn(5, 3)
     sim = cka.CKASimilarity(kernel='rbf', sigma=1e10)
     similarity = sim(pc1, pc2)
-    
-    
+
+    assert similarity is not None
     assert similarity > 0.8
-    
